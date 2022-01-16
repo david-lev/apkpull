4cb4cd27b8104a4c4769fd32e80708cc2984e766
David Lev <davidlev050414@gmail.com>
2022-01-16 10:36:00 +0200

    Fix regex and exit codes

--- apkpull.sh
+++ apkpull.sh
@@ -2,12 +2,10 @@
 
 ########################### APK-PULL | https://github.com/david-lev/apkpull ###########################
 # APKpull is used to create automations for downloading android apps from Google Play and puling them #
-# to a local machine or server as apk files. You can connect several devices at the same time         #
+# to a local machine or server as apk files. You can connect several devices at the same time.        #
 # Usage: apkpull.sh [package-name]; Optionals: [-x or debug] | Created with ❤️ by David Lev            #
 #######################################################################################################
 
-# TODO
-# google account
 
 e='\e[0m'; r='\e[31m'; g='\e[32m'
 y='\e[33m'; b='\e[34m'; p='\e[35m'
@@ -22,12 +20,12 @@ $b                   | |
 $y APK's puller tool $b|_|$y By david-lev$e
 "
 ### VARS & FUNCS ###
-trap "echo; echo 'Exiting from apkpull proccess...'; exit 1" INT
+trap "echo; echo 'Exiting from apkpull proccess...'; exit 30" INT
 [[ "${@}" =~ "-x" ]] && set -x # for debugging
 pkg=${1}
 as="adb shell"
 gp="com.android.vending"
-logs="/tmp/apkpull_log"
+logs_dir="/tmp/apkpull_log"
 langs="en he"
 coins="₪|$"
 allowd_rounds=5
@@ -35,7 +33,6 @@ declare -A buttons_en=( ["open"]="Open" ["play"]="Play" ["install"]="Install" ["
 declare -A buttons_he=( ["open"]="פתח" ["play"]="שחק" ["install"]="התקנה" ["update"]="עדכן" ["cancel"]="ביטול" ["sign_in"]="כניסה" ["hardware"]="המכשיר שלך אינו תואם לגירסה זו." ["country"]="פריט זה אינו זמין בארצך." ["network"]="אין חיבור לאינטרנט" )
 shopt -s extglob
 tmp_file=$(mktemp)
-function split() { aapt d badging ${1} 2>/dev/null | grep -Po "split='\K[^']*"; }
 function error() { echo -e ">> ${r}ERROR: ${1}${2}${e}"; [[ ${3} =~ ^[0-9]{1,3}$ && ${3} -ge 0 && ${3} -le 255 ]] && exit ${3} || true; }
 function print() { echo -e ">> ${1}${2}${e}"; }
 function is_device_connected() { [[ $(adb -s ${device_id} get-state 2>/dev/null) == "device" ]]; }
@@ -50,8 +47,8 @@ function get_button_coords() {
     [[ -z "${coords}" ]] && return 1 || echo ${coords}
 }
 function capture_error() {
-    mkdir -p ${logs}
-    log_name="${logs}/$(date +'%d:%m:%Y_%T')_${device_model}_${device_lang}"
+    mkdir -p ${logs_dir}
+    log_name="${logs_dir}/$(date +'%d:%m:%Y_%T')_${device_model}_${device_lang}"
     cp ${tmp_file} "${log_name}.xml"
     adb -s ${device_id} exec-out screencap -p > "${log_name}.png"
     print ${y} "LOG: Screenshot saved as XML file and PNG to ${g}${log_name}.png ${log_name}.xml"
@@ -59,13 +56,13 @@ function capture_error() {
 
 ### CHECKS ###
 if [[ ${#} < 1  || ${1} == "--help" || ${1} == "-help" ]]; then
-    error ${r} "Package name must be provided! Usage: ${y}apkpull [PACKAGE NAME]${r}" 1
+    error ${r} "Package name must be provided! Usage: ${y}apkpull [PACKAGE NAME]${r}" 10
 elif [[ $(curl --connect-timeout 0.5 -s -o /dev/null -w "%{http_code}" "https://play.google.com/store/apps/details?id=${pkg}") == 404 ]]; then
-    error ${r} "This app doesn't exists in Google Play." 1
+    error ${r} "This app doesn't exists in Google Play." 40
 elif ! command -v adb >/dev/null 2>&1; then
-    error ${r} "Unable to find ADB, please install or add to PATH." 1
+    error ${r} "Unable to find ADB, please install or add to PATH." 20
 elif [[ $(adb devices -l | sed '/List.*\|^$/d; s/\s.*//g' | wc -l) < 1 ]]; then
-    error ${r} "No devices found! At least one device must be connected" 1
+    error ${r} "No devices found! At least one device must be connected" 50
 else
     devices=$(adb devices -l | sed '/List.*\|^$/d; s/\s.*//g')
     print ${g} "$(echo ${devices} | sed 's/\s/\n/g' | wc -l) devices connected!"
@@ -91,7 +88,7 @@ for device_id in ${devices}; do
 
         ### DOWNLOAD ###
         if ! is_unlocked; then
-        print ${r} "The device is locked. Unlocked it to continue..."
+            print ${r} "The device is locked. Unlocked it to continue..."
             while ! is_unlocked; do
                 is_still_connected || continue 2
             done
@@ -144,8 +141,8 @@ for device_id in ${devices}; do
                         is_still_connected || continue 2
                     done
                     print ${g} "The update has started, check the device to see the progress..."
-                    current_vc=$(${as} pm list packages --show-versioncode | grep -w ${pkg} | sed 's/.*versionCode://g')
-                    while [[ ${current_vc} == $(${as} pm list packages --show-versioncode | grep -w ${pkg} | sed 's/.*versionCode://g') ]]; do
+                    current_vc=$(${as} pm list packages --show-versioncode | grep "package:${pkg} " | sed 's/.*versionCode://g')
+                    while [[ ${current_vc} == $(${as} pm list packages --show-versioncode | grep "package:${pkg} " | sed 's/.*versionCode://g') ]]; do
                         is_still_connected || continue 2
                     done
                     print ${g} "The ${y}${pkg}${g} package successfully updated!"
@@ -158,10 +155,10 @@ for device_id in ${devices}; do
         fi
 
         ### PULL ###
-        vc=$(${as} pm list packages --show-versioncode | grep -w ${pkg} | sed 's/.*versionCode://g')
+        vc=$(${as} pm list packages --show-versioncode | grep "package:${pkg} " | sed 's/.*versionCode://g')
         dl="/home/${USER}/Downloads/apkpull_dl/${pkg}/${vc}"
         mkdir -p ${dl}/tmp
-        is_still_connected
+        is_still_connected || continue
         if apk_paths=$(${as} pm path ${pkg} | sed 's/package://g'); then
             print ${g} "Pulling $(echo ${apk_paths} | sed 's/\s/\n/g' | wc -l) files from the device..."
             for apk in ${apk_paths}; do
@@ -190,7 +187,7 @@ for device_id in ${devices}; do
             mv -f !(${pkg}*) ${folder}
             msg="There are $(ls -1 ${folder} | wc -l) splits and one base apk file"
         else # if there is only one apk file
-            test -f ${base} && mv ${base} "${base/_base/}"; base=${base/_base/} # remove "_base" from apk filename
+            test -f ${base} && mv ${base} "${base/_base/}" # remove "_base" from apk filename
         fi
         : $((successful_actions++))
         print ${g} "The operation was completed successfully!. ${msg}"
@@ -198,4 +195,4 @@ for device_id in ${devices}; do
 done
 
 [[ ${successful_actions} == ${actions} ]] && color=${g} || color=${r}
-print ${color} "${successful_actions}/${actions} successful operations!"
+print ${color} "${successful_actions}/${actions} successful operations!" && exit $((actions - successful_actions))
